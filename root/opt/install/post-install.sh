2f73bda101bdafb72d48a0b66401c797cb9d49d6
HeyltsTim <112197473+HeyltsTim@users.noreply.github.com>
2026-06-24 11:09:17 -0400

    Update post-install.sh

--- root/opt/install/post-install.sh
+++ root/opt/install/post-install.sh
@@ -4,7 +4,7 @@ btrfs subvolume create /var/lib/lxd/storage-pools/cachyos
 
 #init
 SERVICES=("syslog-ng" "lxd" "chrony" "dbus" "acpid" "chrony" "firewalld" "sshd" "turnstiled" "elogind" "lm_sensors" "dnsmasq" "fail2ban" "dhcpcd" "apparmor" "zramen")
-for i in {${SERVICES[@]}}; do
+for i in "${SERVICES[@]}"; do
 dinitctl enable $i
 done
 # end of script
