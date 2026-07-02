9d20d7d381e1587f8101e4afe387fed1a715b927
HeyltsTim <112197473+HeyltsTim@users.noreply.github.com>
2026-07-02 15:41:51 -0400

    Delete root/opt/install directory

--- root/opt/install/post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#init
-SERVICES=("syslog-ng" "lxd" "chrony" "dbus" "acpid" "chrony" "firewalld" "sshd" "turnstiled" "elogind" "lm_sensors" "dnsmasq" "fail2ban" "dhcpcd" "apparmor" "zramen")
-for i in "${SERVICES[@]}"; do
-dinitctl enable $i
-done
-# end of script
-rm -rf /opt/install /etc/dinit.d/boot.d/postinstall
