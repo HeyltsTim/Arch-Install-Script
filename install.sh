6da64c1701af6caa1dafc62cdc8076f07c64aa64
HeyltsTim <112197473+HeyltsTim@users.noreply.github.com>
2026-07-04 08:12:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -218,7 +218,10 @@ ${CHRT}"mkinitcpio -P"
 done_msg
 
 echo "networking..."
-${CHRT}"systemctl enable systemd-networkd systemd-resolved reflector lm_sensors gpm sudo_logsrvd systemd-oomd udisks2 paccache.timer reflector.timer fstrim.timer pacman-filesdb-refresh.timer rsyncd sensord apparmor auditd audit-rules dbus-broker dropbear fail2ban shadow smartd systemd-timesyncd systemd-time-wait-sync ufw systemd-hostnamed uuid reflector.timer shadow shadow.timer"
+srv=( "systemd-networkd" "systemd-resolved" "reflector" "lm_sensors" "gpm" "sudo_logsrvd" "systemd-oomd" "udisks2" "paccache.timer" "reflector.timer" "fstrim.timer" "pacman-filesdb-refresh.timer" "rsyncd" "sensord" "apparmor" "auditd" "audit-rules" "dbus-broker" "dropbear" "fail2ban" "shadow" "smartd" "systemd-timesyncd" "systemd-time-wait-sync" "ufw" "systemd-hostnamed" "uuid" "reflector.timer" "shadow" "shadow.timer")
+for i in ${srv[@]}
+${CHRT}"systemctl enable ${i}"
+done
 done_msg
 
 echo "other services..."
